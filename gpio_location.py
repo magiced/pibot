4809d686a0f0bcfd62832840255bcec03d0b35ca
magiced <ed.bisdee@googlemail.com>
2022-04-16 08:52:15 +0100

    first commit from pi

--- gpio_location.py
+++ gpio_location.py
@@ -1,14 +1,30 @@
-import Rpi.GPIO as GPIO
+import RPi.GPIO as GPIO
+ON = True
+OFF = False
+
 
 gpio_locs = {}
+
+gpio_pins = [ 3, 5, 7, 8, 10, 11, 12, 13, 15, 16, 18,
+              19, 21, 22, 23, 24, 26, 29, 31,
+              32, 33, 35, 36, 37, 38, 40]
+
+GPIO.setmode(GPIO.BOARD)
+# GPIO.setwarnings(False)
+
+# setup
+for i in gpio_pins:
+    GPIO.setup(i, GPIO.OUT)
+    GPIO.output(i, OFF)
 
 for i in gpio_pins:
     print(f"GPIO Pin {i}")
     GPIO.output(i,ON)
     x = input(f"    X co-ord->")
     y = input(f"    Y co-ord->")
-    gpio_locs[i] = {'x':x, 'y':y}
-    GPIO.out(i, OFF)
+    name = input(f"    name->")
+    gpio_locs[i] = {'x':x, 'y':y, 'name':name}
+    GPIO.output(i, OFF)
 
-for  key, value in gpio_locs:
-    print(f"{key}: x:{value['x']}, y:{value['y']}")
+for  key, value in gpio_locs.items():
+    print(f"{key}: x:{value['x']}, y:{value['y']}, name:{value['name']}")
---
+++ gpio_sheild_test_1.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as GPIO
+import time
+
+gpio_pins = [ 3, 5, 7, 8, 10, 11, 12, 13, 15, 16, 18,
+              19, 21, 22, 23, 24, 26, 29, 31,
+              32, 33, 35, 36, 37, 38, 40]
+
+GPIO.setmode(GPIO.BOARD)
+# GPIO.setwarnings(False)
+
+# setup
+for i in gpio_pins:
+    GPIO.setup(i, GPIO.OUT)
+
+for i in range(10):
+    for i in gpio_pins:
+        GPIO.output(i, True)
+        time.sleep(0.1)
+        
+    for i in gpio_pins:
+        GPIO.output(i, False)
+        time.sleep(0.1)
+
+# Reset the GPIO pins (turns off motors too)
+GPIO.cleanup()
