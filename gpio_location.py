ee8269b5e77f6abe836c94608c34057e57a22022
magiced <ed.bisdee@googlemail.com>
2022-04-19 07:30:25 +0100

    added a load of motor direction functions to motor_func.py

--- gpio_location.py
+++ gpio_location.py
@@ -2,12 +2,11 @@ import RPi.GPIO as GPIO
 ON = True
 OFF = False
 
-
 gpio_locs = {}
 
-gpio_pins = [ 3, 5, 7, 8, 10, 11, 12, 13, 15, 16, 18,
-              19, 21, 22, 23, 24, 26, 29, 31,
-              32, 33, 35, 36, 37, 38, 40]
+gpio_pins = [ 3,  5,  7,  8,  10, 11, 12, 13, 15,
+              16, 18, 19, 21, 22, 23, 24, 26,
+              29, 31, 32, 33, 35, 36, 37, 38, 40]
 
 GPIO.setmode(GPIO.BOARD)
 # GPIO.setwarnings(False)
--- motor_funcs.py
+++ motor_funcs.py
@@ -4,6 +4,18 @@
 import RPi.GPIO as GPIO  # Import the GPIO Library
 import time  # Import the Time library
 
+def setup_GPIO():
+    # Set the GPIO modes
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+
+    # Set the GPIO Pin mode
+    GPIO.setup(7, GPIO.OUT) # left fed
+    GPIO.setup(8, GPIO.OUT) # left backward
+
+    GPIO.setup(9, GPIO.OUT) # right forward
+    GPIO.setup(10, GPIO.OUT) # right backward
+
 def right_motor_fwd():
     GPIO.output(9, 0)
     GPIO.output(10, 1)
@@ -15,61 +27,74 @@ def right_motor_back():
 def right_motor_off():
     GPIO.output(9, 1)
     GPIO.output(10, 0)
+    
+def left_motor_fwd():
+    GPIO.output(7, 0)
+    GPIO.output(8, 1)
 
+def left_motor_back():
+    GPIO.output(7, 1)
+    GPIO.output(8, 0)
+    
+def left_motor_off():
+    GPIO.output(7, 1)
+    GPIO.output(8, 0)
 
-# Set the GPIO modes
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
+def stop_motors():
+    right_motor_off()
+    left_motor_off()
+    
+def rotate_right(mv_time):
+    right_motor_fwd()
+    left_motor_back()
+    time.sleep(mv_time)
+    stop_motors()
 
-# Set the GPIO Pin mode
-GPIO.setup(7, GPIO.OUT)
-GPIO.setup(8, GPIO.OUT)
-GPIO.setup(9, GPIO.OUT)
-GPIO.setup(10, GPIO.OUT)
+def rotate_left(mv_time):
+    left_motor_fwd()
+    right_motor_back()
+    time.sleep(mv_time)
+    stop_motors()
 
-# Turn all motors off
-GPIO.output(7, 0)
-GPIO.output(8, 0)
-GPIO.output(9, 0) # right motor # 
-GPIO.output(10, 0) # right motor
+def turn_right(mv_time):
+    right_motor_fwd()
+    time.sleep(mv_time)
+    stop_motors()
 
-# Turn the right motor forwards
-GPIO.output(9, 0)
-GPIO.output(10, 1)
+def turn_left(mv_time):
+    left_motor_fwd()
+    time.sleep(mv_time)
+    stop_motors()
 
-# Wait for 1 seconds
-time.sleep(0.5)
+def move_fwd(mv_time):
+    right_motor_fwd()
+    left_motor_back()
+    time.sleep(mv_time)
+    stop_motors()
 
-# stop right motor
-GPIO.output(9, 0)
-GPIO.output(10, 0)
+def move_back(mv_time):
+    right_motor_back()
+    left_motor_back()
+    time.sleep(mv_time)
+    stop_motors()
 
-time.sleep(1)
+def setup_GPIO():
+    # Set the GPIO modes
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
 
-# Turn the left motor forwards
-GPIO.output(7, 0)
-GPIO.output(8, 1)
+    # Set the GPIO Pin mode
+    GPIO.setup(7, GPIO.OUT)
+    GPIO.setup(8, GPIO.OUT)
+    GPIO.setup(9, GPIO.OUT)
+    GPIO.setup(10, GPIO.OUT)
 
-# Wait for 1 seconds
-time.sleep(0.5)
+setup_GPIO()
 
-# stop left motor
-GPIO.output(7, 0)
-GPIO.output(8, 0)
-
-# Wait for 1 seconds
-time.sleep(0.5)
-
-# Turn the right motor forwards
-GPIO.output(9, 0)
-GPIO.output(10, 1)
-
-# Turn the left motor forwards
-GPIO.output(7, 0)
-GPIO.output(8, 1)
-
-# Wait for 1 seconds
-time.sleep(0.5)
+move_fwd(1)
+move_back(1)
+rotate_left(1)
+rotate_right(1)
 
 # Reset the GPIO pins (turns off motors too)
 GPIO.cleanup()
